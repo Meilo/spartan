7e45d3c1636924690f70c913a04bf4764c2b7489
Denetax <mehdistocky@gmail.com>
2015-11-21 12:02:09 +0100

    Maj ajout produit correcte

--- spartan/static/js/app.js
+++ spartan/static/js/app.js
@@ -4,26 +4,30 @@ $( ".add-produit" ).click(function() {
 
 function allowDrop(ev) {
     ev.preventDefault();
-    $("#"+ev.target.id).attr("style","background-color:#F60");
+    $("#"+ev.target.id).attr("style","background-color:#F60;height:60px; width:300px;");
+    $("#"+ev.target.id+"testSpan").attr("style","display:visible; width:100%;text-align:center;padding:6%;");
+
 }
 
 function LeaveCategory(ev) {
     ev.preventDefault();
-    $("#"+ev.target.id).attr("style","background-color:none");
+    $("#"+ev.target.id).attr("style","background-color:none;height:60px; width:300px;");
 }
 
 function drag(ev) {
+    console.log($('#'+ev.target.id).children().text());
     ev.dataTransfer.setData("text", $('#'+ev.target.id).children().text());
+    $( ".DropZoneHide" ).addClass( "DropZone" );
+    $( ".DropZoneHide" ).removeClass( "DropZoneHide")
 }
 
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    console.log(ev.target.id);
-    console.log(data);
-    console.log( $("#"+ev.target.id));
-    $("#"+ev.target.id).prepend("<p>"+data+"<p>");
-    $("#"+ev.target.id).attr("style","background-color:none");
+    console.log("#"+ev.target.id+"Drop");
+    $("#"+ev.target.id+"Drop").prepend("<div class=\"produit\"><p>"+data+"<p></div>");
+    $( ".DropZone" ).addClass( "DropZoneHide" );
+    $( ".DropZone" ).removeClass( "DropZone")
 }
 
 
